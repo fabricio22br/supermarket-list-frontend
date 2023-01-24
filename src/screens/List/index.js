432eb647bd0d4c0eeebd992a242615ea3b43949f
fabricio22br <fabriciocostaodonto@gmail.com>
2023-01-24 17:03:33 -0300

    feat: initial commit

--- src/screens/List/index.js
+++ src/screens/List/index.js
@@ -1,3 +1,15 @@
+import { useEffect } from 'react'
+import { getList } from '../../services/request'
+
 export const ListScreen = () => {
+  const loadListItems = async () => {
+    const result = await getList()
+    console.log({ result })
+  }
+
+  useEffect(() => {
+    loadListItems()
+  }, [])
+
   return <div></div>
 }
