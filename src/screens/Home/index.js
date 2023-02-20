94879d7a51752cedd83aa9679d61e223c96d1434
fabricio22br <fabriciocostaodonto@gmail.com>
2023-02-20 11:00:39 -0300

    feat: interactive for mobile

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,8 +1,15 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import './index.css'
 import { Button, Input } from 'components'
 import { SAVE_USERNAME_PATH } from 'services/constants'
+import {
+  ScreenContainer,
+  ContentContainer,
+  BagImage,
+  Title,
+  SubTitle,
+  ButtonContainer
+} from './styles'
 
 export const HomeScreen = () => {
   const navigate = useNavigate()
@@ -18,23 +25,17 @@ export const HomeScreen = () => {
   }
 
   return (
-    <div className="home-screen-container">
-      <div className="home-screen-content-container">
-        <img
-          className="shopping-bag-image"
-          src="/images/shopping-bag.png"
-          alt="shopping-bag"
-        />
-        <h1 className="home-screen-title">
-          Sua lista de supermercado mais fácil do que nunca
-        </h1>
-        <h3 className="home-screen-subtitle">
+    <ScreenContainer>
+      <ContentContainer>
+        <BagImage />
+        <Title>Sua lista de supermercado mais fácil do que nunca</Title>
+        <SubTitle>
           Ajudamos você a organizar sua lista de compras de forma descomplicada.
-        </h3>
+        </SubTitle>
 
-        <h3 className="home-screen-subtitle-description">
+        <SubTitle mw={452} align="left" mb={16}>
           Digite abaixo seu usuário para ter acesso a sua lista de compras:
-        </h3>
+        </SubTitle>
 
         <Input
           onChange={(text) => setUsername(text)}
@@ -42,10 +43,10 @@ export const HomeScreen = () => {
           label="Username"
           placeholder="Ex: usuario1"
         />
-        <div className="home-screen-button-container">
+        <ButtonContainer>
           <Button onClick={onClickContinue}>Continuar</Button>
-        </div>
-      </div>
-    </div>
+        </ButtonContainer>
+      </ContentContainer>
+    </ScreenContainer>
   )
 }
